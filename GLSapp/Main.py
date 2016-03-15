69786dbb9b95330bf5905399274d0c4f03c03cb4
Loic Teixeira <loic@springload.co.nz>
2016-03-16 08:36:32 +1300

    Allow shorthand p option to accept a value

--- GLSapp/Main.py
+++ GLSapp/Main.py
@@ -10,58 +10,58 @@ from GLSapp.Scraper import Scraper
 
 # Application handling class
 class MainApp():
-    
+
     def __init__(self):
-        self.scraper = Scraper() 
+        self.scraper = Scraper()
         self.pattern = ""
-        
+
         self.fileUpdated = False
-    
+
     # Main application entry point
     def main(self):
-        
+
         try:
-            opts, args = getopt.getopt(sys.argv[1:], "h:f:u:p", ["help", "file=", "url=", "pattern="])
+            opts, args = getopt.getopt(sys.argv[1:], "h:f:u:p:", ["help", "file=", "url=", "pattern="])
         except getopt.GetoptError as err:
             # print help information and exit:
             print(err) # will print something like "option -a not recognized"
             self.usage()
             sys.exit(2)
-            
+
         for o, a in opts:
             if o in ("-h", "--help"):
                 self.usage()
                 sys.exit()
-            
+
             if o in ("-p", "--pattern"):
                 self.pattern = str(a)
-            
+
             elif o in ("-f", "--file"):
                 _urlList = self.readFile(a)
                 if len(_urlList) > 0:
                     self.scraper.addPageList(_urlList)
-    
+
             elif o in ("-u", "--url"):
                 self.scraper.addPage(a)
-        
+
         if (len(self.pattern) <= 0):
             self.usage()
             sys.exit()
-        
+
         self.scraper.run(self.pattern, self.writeCSV)
-    
-    # Write output into file 
+
+    # Write output into file
     def writeCSV(self, _results):
         if self.fileUpdated == False:
             self.fileUpdated = True
-            
+
             fh = open("Results.txt", "w")
             fh.write('\n'.join(_results) + '\n')
             fh.close()
-            
+
             print("Result written!")
-    
-    
+
+
     # Display help message
     def usage(self):
         print("Options:")
@@ -73,7 +73,7 @@ class MainApp():
 
     # Read list from CSV file
     def readFile(self, _filename):
-        
+
         if os.path.isfile(_filename):
             csv_list = []
             with open(_filename, 'r') as f:
@@ -81,15 +81,15 @@ class MainApp():
                 for line in reader:
                     if ("http" in line[0]):
                         csv_list.append(line[0])
-    
+
             return csv_list
-        
+
         else:
             print("File: "+ _filename + " not found")
-         
+
         return False
 
 # Run application from self
 if __name__ == '__main__':
     _app = MainApp()
-    _app.main() 
+    _app.main()
