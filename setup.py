b313b5936912687fe6d228d5f11863125bca3aa6
Loic Teixeira <loic@springload.co.nz>
2016-03-16 08:49:35 +1300

    Update setup.py
     - `requests` is a dependency not an internal package.
     - `unittest` is a core module of python, so installing it as a dependency fails.

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ config = {
     'url': 'URL to get it at.',
     'download_url': 'Where to download it.',
     'author_email': 'dave@springload.co.nz',
-    'version': '0.1',
-    'install_requires': ['unittest'],
-    'packages': ['requests'],
+    'version': '0.2',
+    'install_requires': ['requests'],
+    'packages': ['GLSapp'],
     'scripts': [],
     'name': 'GLSapp'
 }
